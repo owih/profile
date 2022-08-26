02ccb8ee79b0abcbb58c3d28b50b174cf62ad9dd
golovatenko <golovatenko@intervolga.ru>
2022-08-26 05:22:24 +0300

    [front] fix

--- src/phpmailer/mail.php
+++ src/phpmailer/mail.php
@@ -33,7 +33,7 @@ try {
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'golovatenko.dmitry@gmail.com'; // Логин на почте
-    $mail->Password   = 'gnnsmnthaynrqkqm'; // Пароль на почте
+    $mail->Password   = ''; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
 
